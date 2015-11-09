338ad9c46cf413022784c03e8cd190a86efbeca7
paya-tan <cru5n1k.n4n0@gmail.com>
2015-11-08 23:05:56 -0800

    Create main.cc

---
+++ main.cc
@@ -0,0 +1,13 @@
+#include "compressionhelper.h"
+#include <gtkmm/application.h>
+
+int main (int argc, char *argv[])
+{
+  Glib::RefPtr<Gtk::Application> app = 
+  Gtk::Application::create(argc, argv, "org.gtkmm.example");
+
+  compressionHelper compressionHelper;
+
+  //Shows the window and returns when it is closed.
+  return app->run(compressionHelper);
+}
