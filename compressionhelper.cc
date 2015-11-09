aed29d8991f6c978449febfcf9c095d34de13dfe
paya-tan <cru5n1k.n4n0@gmail.com>
2015-11-09 04:23:57 -0800

    Update compressionhelper.cc

--- compressionhelper.cc
+++ compressionhelper.cc
@@ -6,18 +6,18 @@
 #include <cairomm/context.h>
 #include <iostream>
 #include <sstream>
+#include <algorithm>
 
 
 compressionHelper::compressionHelper()
-: import_button("Import"),
-export_button("Export"),
+: import_button("Convert!"),
 normal_map_selector("Normal Map"),
 advanced_label("Advanced"),
 main_box(Gtk::ORIENTATION_VERTICAL)
 {
   set_border_width(5);
   set_title("Compression Helper V0.1");
-  set_default_size(640,480);
+  // set_default_size(640,480);
 
   add(main_box);
 
@@ -27,23 +27,37 @@ main_box(Gtk::ORIENTATION_VERTICAL)
   import_button.signal_clicked().connect(sigc::mem_fun(*this,
               &compressionHelper::on_import));
 
-  import_button.set_tooltip_text("Select a URTW list file for conversion.");
-
-  export_button.signal_clicked().connect(sigc::mem_fun(*this,
-              &compressionHelper::on_export));
-
-  export_button.set_tooltip_text("Exports the images.");
+  import_button.set_tooltip_text("Select a URTW list file for conversion and converts it.");
 
   inoutbtn_box.pack_start(import_button, Gtk::PACK_SHRINK, 20);
-  inoutbtn_box.pack_start(export_button, Gtk::PACK_SHRINK, 20);
 
   normal_map_selector.signal_clicked().connect(sigc::mem_fun(*this,
               &compressionHelper::on_normal_checked));
 
 
-  adv_grid.add(advanced_label);
+  m_refTreeModel = Gtk::ListStore::create(m_Columns);
+  select_cformat.set_model(m_refTreeModel);
 
-  adv_grid.add(normal_map_selector);
+  Gtk::TreeModel::Row row = *(m_refTreeModel->append());
+  row[m_Columns.m_col_id] = 1;
+  row[m_Columns.m_col_name] = "DXT1 (BC1)";
+
+  row = *(m_refTreeModel->append());
+  row[m_Columns.m_col_id] = 2;
+  row[m_Columns.m_col_name] = "DXT1a (BC1a)";
+
+  row = *(m_refTreeModel->append());
+  row[m_Columns.m_col_id] = 3;
+  row[m_Columns.m_col_name] = "DXT3 (BC3)";
+
+  select_cformat.pack_start(m_Columns.m_col_id);
+  select_cformat.pack_start(m_Columns.m_col_name);
+
+  adv_grid.add(advanced_label);
+  adv_grid.attach_next_to(select_cformat, advanced_label, Gtk::POS_BOTTOM, 2, 1);
+  adv_grid.attach(normal_map_selector, 2, 1, 2, 2);
+
+  select_cformat.signal_changed().connect( sigc::mem_fun(*this, &compressionHelper::on_cselect));
 
   show_all_children();
 }
@@ -60,6 +74,18 @@ void compressionHelper::on_normal_checked()
     nm_flag = true;
 }
 
+void compressionHelper::on_cselect()
+{
+  Gtk::TreeModel::iterator iter = select_cformat.get_active();
+  if(iter)
+  {
+    Gtk::TreeModel::Row row = *iter;
+    if(row)
+    {
+      int compress_select = row[m_Columns.m_col_id];
+    }
+  }
+}
 
 void compressionHelper::on_import()
 {
@@ -102,13 +128,6 @@ void compressionHelper::on_import()
   }
 }
 
-void compressionHelper::on_export()
-{
-  std::cout << "hi";
-}
-
-
-
 void compressionHelper::image_checker(std::string inputFile)
 {
   using namespace std;
@@ -134,10 +153,10 @@ void compressionHelper::image_checker(std::string inputFile)
       string raw_image = raw_data[i].substr(12);
       cout << "marker: " << marker << endl << "width: " << width << endl << "height: " << height << endl;
 
-      vector<string> textels;
+      vector<char> textels;
       for (int x=0; x<raw_image.length(); x+=4)
       {
-        textels.push_back(raw_image.substr(x,4));
+        textels.push_back(raw_image[x+3]);
       }
 
       bool A = true;
@@ -148,36 +167,38 @@ void compressionHelper::image_checker(std::string inputFile)
       if ( length_of_textel_list < 36)
       {
         numitter = length_of_textel_list;
-        cout << numitter;
       }
       for (int z=0; z < numitter; z++)
       {
         //check alpha to see if it's binary or changes at all over the whole file.
-        for (int b=0; b<numitter; b++)
+        for (unsigned int b=0; b<numitter; b++)
         {
-          if (textels[z].substr(3,1).compare(textels[b].substr(3,1)) != 0)
+          if (textels[z] != textels[z+1])
           {
-            A = true;
+            A = false;
           }
           else
           {
-            string input = textels[z].substr(3,1);
-            if (input.compare(std::string("\0x00", 1)) != 0 or input.compare(std::string("\xff", 1)) != 0)
+            // string input = textels[z].substr(3,1);
+            if (textels[z] != static_cast<char>(0x00) and textels[z] != static_cast<char>(0xff))
               CHK_IMAGE = false;
           }
         }
       }
-      for (int a=0; a<raw_image.size(); a+=4)
-      {
-        char Alpha = raw_image[a];
-        char R = raw_image[a+1];
-        char G = raw_image[a+2];
-        char B = raw_image[a+3];
-        raw_image[a] = B;
-        raw_image[a+1] = G;
-        raw_image[a+2] = R;
-        raw_image[a+3] = Alpha;
-      }
+
+      // for specific channel switching or reversing (as in bmp)
+      // for (int a=0; a<raw_image.size(); a+=4)
+      // {
+      //   char Alpha = raw_image[a];
+      //   char R = raw_image[a+1];
+      //   char G = raw_image[a+2];
+      //   char B = raw_image[a+3];
+      //   raw_image[a] = B;
+      //   raw_image[a+1] = G;
+      //   raw_image[a+2] = R;
+      //   raw_image[a+3] = Alpha;
+      // }
+      // std::reverse(raw_image.begin(), raw_image.end());
       to_compress(raw_image, width, height, A, CHK_IMAGE);
     }
   }
@@ -230,34 +251,53 @@ std::vector<std::string> compressionHelper::URTW_parser(std::string realFile)
   }
 }
 
-void compressionHelper::to_compress(std::string data, int width, int height, bool A, bool CHK_IMAGE)
+void compressionHelper::to_compress(std::string& data, int width, int height, bool A, bool CHK_IMAGE)
 {
   // using namespace nvtt;
   nvtt::InputOptions inputOptions;
   inputOptions.setTextureLayout(nvtt::TextureType_2D, width, height, 1);
   inputOptions.setMipmapData(data.c_str(), width, height);
+  std::cout << "made it" << std::endl;
   inputOptions.setMipmapGeneration(false, -1);
-  // inputOptions.setFormat(nvtt::InputFormat_BGRA_8UB);
+  inputOptions.setFormat(nvtt::InputFormat_BGRA_8UB);
 
   nvtt::OutputOptions outputOptions;
   outputOptions.setFileName("output.dds");
 
   nvtt::CompressionOptions compressionOptions;
-  if (CHK_IMAGE = true)
-    compressionOptions.setFormat(nvtt::Format_BC1);
-  else
+  if (compress_select != 0)
   {
-    if (A = true)
-    {
-      inputOptions.setAlphaMode(nvtt::AlphaMode_Transparency);
-      compressionOptions.setFormat(nvtt::Format_BC1a);
-    }
+    if (compress_select == 1)
+      compressionOptions.setFormat(nvtt::Format_BC1);
     else
     {
-      if (nm_flag)
-        compressionOptions.setFormat(nvtt::Format_BC3n);
+      if (compress_select == 2)
+        compressionOptions.setFormat(nvtt::Format_BC1a);
       else
-        compressionOptions.setFormat(nvtt::Format_BC3);
+      {
+        if (compress_select == 3)
+          compressionOptions.setFormat(nvtt::Format_BC3);
+      }
+    }
+  }
+  else
+  {
+    if (CHK_IMAGE = true)
+      compressionOptions.setFormat(nvtt::Format_BC3);
+    else
+    {
+      if (A = true)
+      {
+        inputOptions.setAlphaMode(nvtt::AlphaMode_Transparency);
+        compressionOptions.setFormat(nvtt::Format_BC1a);
+      }
+      else
+      {
+        if (nm_flag)
+          compressionOptions.setFormat(nvtt::Format_BC3n);
+        else
+          compressionOptions.setFormat(nvtt::Format_BC3);
+      }
     }
   }
   nvtt::Compressor compressor;
@@ -268,4 +308,5 @@ void compressionHelper::to_compress(std::string data, int width, int height, boo
 
 //STILL NEEDS TO BE DONE
 //   ADD IN INFO IN UI ABOUT SOME DXT VS OTHERS AND
-          //EXPLICIT COMPRESSION SELECTION
+          //NEEDS FIX FOR LARGER FILES
+          //POSSIBLY ADD IN SUPPORT FOR OTHER FORMATS
